6e0bc0e66c19e7bf4644d36a6208a32f2e63da35
R1ch1131 <cfyz1553@mail.ru>
2024-05-25 13:30:37 +0900

    fdsf

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,10 +1,12 @@
 
-
 export const Header = () => {
     return(
         
             <div className=" h-16 w-full bg-blue-600 flex justify-between text-white fixed top-0 z-50">
-                <a href="#" className="mt-4 ml-12">иконка</a>
+                <div>
+                <img className='w-5 h-5' src={require("./assets/icon4.png")} alt="icon" />
+                <p>Omsu Network</p>
+                </div>
                 <a href="##" className="mt-4">ping</a>
                 <a href="###" className="mt-4">stats</a>
                 <a href="####" className="mt-4 mr-12">profile</a>
